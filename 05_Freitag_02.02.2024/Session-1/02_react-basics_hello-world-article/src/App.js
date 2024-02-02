4424eaa5b3a24d17d5086575c0d73ffbdb1dfb60
Marcel Epp <marcel.epp@tutanota.de>
2024-02-02 11:19:59 +0100

    add fixes

---
+++ 05_Freitag_02.02.2024/Session-1/02_react-basics_hello-world-article/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>This is my component!</h1>
+      <p>I am a text...</p>
+    </article>
+  );
+}
+
+export default function App() {
+  return HelloWorldArticle();
+}
