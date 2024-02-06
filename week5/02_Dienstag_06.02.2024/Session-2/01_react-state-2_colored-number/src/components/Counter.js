d69779bdd8d40347de7119f9e0652317203334c5
Marcel Epp <epp.marcel@icloud.com>
2024-02-06 16:21:59 +0100

    add fixes

---
+++ week5/02_Dienstag_06.02.2024/Session-2/01_react-state-2_colored-number/src/components/Counter.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import ColoredNumber from "./ColoredNumber";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="counter">
+      <h1>Colored Number</h1>
+      <ColoredNumber value={count} />
+      <div className="counter__buttons-container">
+        <button
+          onClick={() => {
+            setCount(count + 1);
+            console.log(count + 1);
+          }}
+          type="button"
+          className="counter__button"
+          aria-label="increment count"
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setCount(count - 1);
+            console.log(count - 1);
+          }}
+          type="button"
+          className="counter__button"
+          aria-label="decrement count"
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
