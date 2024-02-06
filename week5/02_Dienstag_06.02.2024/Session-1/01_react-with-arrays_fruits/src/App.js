7b7427845cfdfea9b54628683e0417fe4742c057
Marcel Epp <epp.marcel@icloud.com>
2024-02-06 10:19:54 +0100

    add fixes

---
+++ week5/02_Dienstag_06.02.2024/Session-1/01_react-with-arrays_fruits/src/App.js
@@ -0,0 +1,19 @@
+import Card from "./components/Card";
+
+export default function App() {
+  const fruits = [
+    { id: 0, name: "🍌 banana", color: "yellow" },
+    { id: 1, name: "🍏 apple", color: "green" },
+    { id: 2, name: "🍒 cherry", color: "red" },
+    { id: 3, name: "🥭 mango", color: "green" },
+    { id: 4, name: "🍇 grape", color: "purple" },
+  ];
+
+  return (
+    <div className="app">
+      {fruits.map(({ id, name }) => (
+        <Card key={id} name={name} />
+      ))}
+    </div>
+  );
+}
