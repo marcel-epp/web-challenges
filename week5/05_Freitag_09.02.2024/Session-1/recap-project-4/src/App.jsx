891371e772939a5f76ca69e90a2064c84613c2eb
Marcel Epp <epp.marcel@icloud.com>
2024-02-09 10:52:18 +0100

    add initial setup

---
+++ week5/05_Freitag_09.02.2024/Session-1/recap-project-4/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Weather App!</h1>
+        <p>This is the weather app for recap project 4.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
