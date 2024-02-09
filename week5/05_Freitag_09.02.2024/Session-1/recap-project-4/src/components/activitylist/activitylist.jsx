cc659775242c7c1ea38a548fdb99f8e234709b9a
Marcel Epp <epp.marcel@icloud.com>
2024-02-09 16:12:44 +0100

    add filtered list

--- week5/05_Freitag_09.02.2024/Session-1/recap-project-4/src/components/activitylist/activitylist.jsx
+++ week5/05_Freitag_09.02.2024/Session-1/recap-project-4/src/components/activitylist/activitylist.jsx
@@ -1,6 +1,12 @@
-function ActivityList({ activitys }) {
+function ActivityList({ activitys, isGoodWeather }) {
   return (
     <div className="activity--list">
+      <h2>
+        {isGoodWeather
+          ? "The weather is awesome! Go outside and:"
+          : "Bad weather outside! Here's what you can do now:"}
+      </h2>
+
       <ol>
         {activitys.map((activity) => (
           <li key={activity.id}>{activity.name}</li>
