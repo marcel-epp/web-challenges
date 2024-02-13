c2a71ac9b81114030aea65aefe0854a299ddd9e9
Marcel Epp <epp.marcel@icloud.com>
2024-02-13 16:22:54 +0100

    add fixes

---
+++ week6/02_Dienstag_13.02.2024/Session-2/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/[slug].js
@@ -0,0 +1,55 @@
+import { useRouter } from "next/router";
+import Link from "next/link.js";
+import Image from "next/image";
+import { volumes } from "../../lib/data.js";
+
+export default function Volumes() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const volumeIndex = volumes.findIndex((book) => book.slug === slug);
+  const volume = volumes[volumeIndex];
+  const nextVolume = volumes[volumeIndex + 1];
+  const previousVolume = volumes[volumeIndex - 1];
+
+  if (!volume) {
+    return null;
+  }
+
+  const { title, description, cover, books } = volume;
+
+  return (
+    <>
+      <Link href="/volumes">← All Volumes</Link>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <ul>
+        {books.map(({ ordinal, title }) => (
+          <li key={title}>
+            {ordinal}: <strong>{title}</strong>
+          </li>
+        ))}
+      </ul>
+      <Image
+        src={cover}
+        alt={`Cover image of ${title}`}
+        width={140}
+        height={230}
+      />
+      <div>
+        {previousVolume ? (
+          <Link href={`/volumes/${previousVolume.slug}`}>
+            ← Previous Volume: {previousVolume.title}
+          </Link>
+        ) : null}
+      </div>
+      <div>
+        {nextVolume ? (
+          <Link href={`/volumes/${nextVolume.slug}`}>
+            Next Volume: {nextVolume.title} →
+          </Link>
+        ) : null}
+      </div>
+    </>
+  );
+}
