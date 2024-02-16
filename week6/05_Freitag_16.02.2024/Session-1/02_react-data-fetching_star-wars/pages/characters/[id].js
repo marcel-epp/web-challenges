8afa9ff21bb5fb372f2a47c20fea687fe49b905e
Marcel Epp <epp.marcel@icloud.com>
2024-02-16 12:05:20 +0100

    add fixes

---
+++ week6/05_Freitag_16.02.2024/Session-1/02_react-data-fetching_star-wars/pages/characters/[id].js
@@ -0,0 +1,38 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, error, isLoading } = useSWR(
+    `https://swapi.dev/api/people/${id}`,
+    fetcher
+  );
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>Ops, something went wring! Contant Support!</h1>;
+  }
+
+  //const id = 1;
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
+      />
+    </Layout>
+  );
+}
