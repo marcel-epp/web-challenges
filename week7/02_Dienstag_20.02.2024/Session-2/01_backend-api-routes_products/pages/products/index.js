4556c68da1f7c5e83d29ff0e3d893ff3807d8749
Marcel Epp <epp.marcel@icloud.com>
2024-02-20 16:39:42 +0100

    add fixes

---
+++ week7/02_Dienstag_20.02.2024/Session-2/01_backend-api-routes_products/pages/products/index.js
@@ -0,0 +1,35 @@
+import useSWR from "swr";
+import Link from "next/link";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function Products() {
+  const { data, isLoading, error } = useSWR(
+    "http://localhost:3000/api/products",
+    fetcher
+  );
+
+  if (isLoading) return <div>loading...</div>;
+  if (error) return <div>failed to load</div>;
+
+  // render data
+  return (
+    <>
+      <Link href="/api/products">/api/products</Link>
+      <br />
+      <Link href="/api/products/1">/api/products/1</Link>
+      {data.map(({ id, name, description, price, currency, category }) => (
+        <div key={id}>
+          <h2>{name}</h2>
+          <ul>
+            <li>{description}</li>
+            <li>
+              {price} {currency}
+            </li>
+            <li>{category}</li>
+          </ul>
+        </div>
+      ))}
+    </>
+  ); // Hello Luke Skywalker!
+}
